4db40efd123248d8082a1a6cc248f65599e6d6e1
NVTuann201 <nvtuann201@gmail.com>
2023-05-24 17:22:39 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,128 @@
+<template>
+  <v-container class="main fill-height pa-3 flex-column">
+    <v-layout class="recent-workspace flex-column">
+      <v-container>
+        <div>Recent workspaces</div>
+      </v-container>
+      <v-layout>
+        <v-card class="card">
+          <div class="card-info">Name1</div>
+          <div class="date">date1</div>
+          <div class="items-count">20 items</div>
+          <div class="owner">Owner: Owner</div>
+          <div class="size">Size: 400MB</div>
+        </v-card>
+        <v-card class="card">
+          <div class="card-info">Name1</div>
+          <div class="date">date1</div>
+          <div class="items-count">20 items</div>
+          <div class="owner">Owner: Owner</div>
+          <div class="size">Size: 400MB</div>
+        </v-card>
+        <v-card class="card">
+          <div class="card-info">Name1</div>
+          <div class="date">date1</div>
+          <div class="items-count">20 items</div>
+          <div class="owner">Owner: Owner</div>
+          <div class="size">Size: 400MB</div>
+        </v-card>
+        <v-card class="card">
+          <div class="card-info">Name1</div>
+          <div class="date">date1</div>
+          <div class="items-count">20 items</div>
+          <div class="owner">Owner: Owner</div>
+          <div class="size">Size: 400MB</div></v-card
+        >
+      </v-layout>
+    </v-layout>
+    <v-layout class="all-workspace flex-column">
+      <v-container class="d-flex flex-row">
+        <div class="ma-2">Order By</div>
+        <div class="ma-2">Date</div>
+        <div class="ma-2">Name</div>
+        <div class="ma-2">Size</div>
+        <v-spacer></v-spacer>
+        <v-text-field single-line></v-text-field>
+      </v-container>
+      <v-layout>
+        <v-card class="card">
+          <div class="card-info">Name1</div>
+          <div class="date">date1</div>
+          <div class="items-count">20 items</div>
+          <div class="owner">Owner: Owner</div>
+          <div class="size">Size: 400MB</div></v-card
+        >
+        <v-card class="card">
+          <div class="card-info">Name1</div>
+          <div class="date">date1</div>
+          <div class="items-count">20 items</div>
+          <div class="owner">Owner: Owner</div>
+          <div class="size">Size: 400MB</div></v-card
+        >
+        <v-card class="card">
+          <div class="card-info">Name1</div>
+          <div class="date">date1</div>
+          <div class="items-count">20 items</div>
+          <div class="owner">Owner: Owner</div>
+          <div class="size">Size: 400MB</div></v-card
+        ><v-card class="card">
+          <div class="card-info">Name1</div>
+          <div class="date">date1</div>
+          <div class="items-count">20 items</div>
+          <div class="owner">Owner: Owner</div>
+          <div class="size">Size: 400MB</div></v-card
+        >
+      </v-layout>
+    </v-layout>
+  </v-container>
+</template>
+
+<script setup>
+import axios from "axios";
+</script>
+<script>
+export default {
+  name: "HelloWorld",
+  data: () => ({
+    workspaces: [],
+  }),
+  methods: {
+    getWorkspaces() {
+      axios
+        .get("https://api-aws.eofactory.ai/api/workspaces?region=sea")
+        .then((response) => {
+          this.workspaces = response.data;
+        });
+    },
+  },
+  mounted() {
+    this.workspaces = this.getWorkspaces();
+  },
+};
+</script>
+<style scoped>
+.recent-workspace {
+  margin: 28px 4px 4px 4px;
+  width: 100%;
+  flex-wrap: wrap;
+}
+.all-workspace {
+  flex-wrap: wrap;
+  width: 100%;
+  margin: 56px 4px 4px 4px;
+}
+.main {
+  width: 100% !important;
+}
+.card {
+  width: 300px;
+  height: 200px;
+  margin: 4px;
+  display: flex;
+  flex-direction: column;
+  padding: 12px 16px 0;
+}
+.v-text-field {
+  max-width: 300px;
+}
+</style>
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-title>
+      <v-layout class="header">
+        <div class="logo">
+          <v-img
+            src="https://cdn.eofactory.ai/unsafe/0x45/https://eofactory.ai/images/logo/EOfactory-logo.png?ver=1.0.0"
+          />
+        </div>
+
+        <v-layout class="header-content">
+          <v-text-field
+            density="compact"
+            hide-details
+            single-line
+            variant="solo"
+            prepend-inner-icon="mdi-magnify"
+          ></v-text-field>
+          <div class="pa-2">EOToken: 250</div>
+          <div>
+            <v-btn icon="mdi-help-circle-outline"></v-btn>
+            <v-btn icon="mdi-bell-outline"></v-btn>
+          </div>
+          <v-select density="compact" variant="solo" hide-details></v-select>
+          <div class="pa-2">
+            <v-layout>
+              <v-icon>mdi-account</v-icon>
+              <span>NVTuann</span>
+            </v-layout>
+          </div>
+        </v-layout>
+      </v-layout>
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script setup>
+//
+</script>
+<style scoped>
+.logo {
+  height: 60px;
+  width: 120px;
+  margin-right: 10px;
+}
+.header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100% !important;
+}
+.v-text-field {
+  max-width: 450px !important;
+}
+.v-select {
+  max-width: 150px !important;
+}
+.header-content{
+  max-width: 1200px;
+  justify-content: space-between;
+}
+</style>
